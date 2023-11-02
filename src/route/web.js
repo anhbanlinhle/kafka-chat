9ca8a395695cd8edf783c672c72884f98dffd7b4
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-03 00:47:53 +0700

    add router for test request

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,8 @@ let router = express.Router()
 const initWebRoute = (app) => {
   router.get('/', homeController.getHomePage)
 
+  router.get('/test', homeController.firstRequest)
+
   return app.use('/', router)
 }
 
