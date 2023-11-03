cb5725a9e2e34190b3f2b4a3f76ee034da798fbc
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-04 04:12:11 +0700

    add consumer for MQ

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 import express from 'express'
 import initWebRoute from './route/web.js'
+import KafkaConfig from './configs/connectMQ'
 
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 1111
+const kafkaConfig = new KafkaConfig()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 initWebRoute(app)
+
+kafkaConfig.consume("my-topic", (value) => {
+  console.log("📨 : ", value)
+})
 
 app.listen(port, () => {
   console.log(`\nApp running at: \n\t\thttp://localhost:${port}`)
