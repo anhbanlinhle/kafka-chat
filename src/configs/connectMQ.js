3e30198fb5f5a412e5b3da2c98bf340d7ddcc6d9
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-03 15:10:15 +0700

    add kafka config constructor

--- src/configs/connectMQ.js
+++ src/configs/connectMQ.js
@@ -1,7 +1,14 @@
 import { Kafka } from "kafkajs"
 
 class KafkaConfig {
-
+  constructor() {
+    this.kafka = new Kafka({
+      clientId: "stellaron",
+      brokers: ["localhost:9091"],
+    });
+    this.producer = this.kafka.producer()
+    this.consumer = this.kafka.consumer({ groupId: "blade" })
+  }
 }
 
 export default KafkaConfig;
